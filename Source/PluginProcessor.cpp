5611b691a57d4c34e555d98869db1f5c541bce78
0x01 <zach.d.howard@gmail.com>
2022-02-16 14:44:25 -0800

    Refactored Params to work with multiple bands

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -22,16 +22,32 @@ BasicMBCompAudioProcessor::BasicMBCompAudioProcessor()
                        )
 #endif
 {
-    compressor.attack = dynamic_cast<AudioParameterFloat*>(apvts.getParameter("Attack"));
-    jassert(compressor.attack != nullptr);
-    compressor.release = dynamic_cast<AudioParameterFloat*>(apvts.getParameter("Release"));
-    jassert(compressor.release != nullptr);
-    compressor.threshold = dynamic_cast<AudioParameterFloat*>(apvts.getParameter("Threshold"));
-    jassert(compressor.threshold != nullptr);
-    compressor.ratio = dynamic_cast<AudioParameterChoice*>(apvts.getParameter("Ratio"));
-    jassert(compressor.ratio != nullptr);
-    compressor.bypassed = dynamic_cast<AudioParameterBool*>(apvts.getParameter("Bypassed"));
-    jassert(compressor.bypassed != nullptr);
+    using namespace Params;
+    const auto& params = GetParams();
+
+    auto floatHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+    {
+        param = dynamic_cast<AudioParameterFloat*>(apvts.getParameter(params.at(paramName)));
+        jassert(param != nullptr);
+    };
+
+    auto choiceHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+    {
+        param = dynamic_cast<AudioParameterChoice*>(apvts.getParameter(params.at(paramName)));
+        jassert(param != nullptr);
+    };
+
+    auto boolHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+    {
+        param = dynamic_cast<AudioParameterBool*>(apvts.getParameter(params.at(paramName)));
+        jassert(param != nullptr);
+    };
+
+    floatHelper(compressor.attack, Names::Attack_Low_Band);
+    floatHelper(compressor.release, Names::Release_Low_Band);
+    floatHelper(compressor.threshold, Names::Threshold_Low_Band);
+    choiceHelper(compressor.ratio, Names::Ratio_Low_Band);
+    boolHelper(compressor.bypassed, Names::Bypassed_Low_Band);
 }
 
 BasicMBCompAudioProcessor::~BasicMBCompAudioProcessor()
@@ -201,15 +217,18 @@ void BasicMBCompAudioProcessor::setStateInformation (const void* data, int sizeI
 AudioProcessorValueTreeState::ParameterLayout BasicMBCompAudioProcessor::createParameterLayout() {
     APVTS::ParameterLayout layout;
 
-    layout.add(std::make_unique<AudioParameterFloat>("Threshold", 
-        "Threshold", 
+    using namespace Params;
+    const auto& params = GetParams();
+
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Threshold_Low_Band),
+        params.at(Names::Threshold_Low_Band),
         NormalisableRange<float>(-60, 12, 1, 1),
         0));
 
     auto attackReleaseRange = NormalisableRange<float>(5, 500, 1, 1);
     
-    layout.add(std::make_unique<AudioParameterFloat>("Attack", "Attack", attackReleaseRange, 50));
-    layout.add(std::make_unique<AudioParameterFloat>("Release", "Release", attackReleaseRange, 250));
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Attack_Low_Band), params.at(Names::Attack_Low_Band), attackReleaseRange, 50));
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Release_Low_Band), params.at(Names::Release_Low_Band), attackReleaseRange, 250));
 
     auto choices = std::vector<double>{ 1,1.5,2,3,4,5,6,7,8,10,15,20,50,100 };
     StringArray sa;
@@ -217,9 +236,9 @@ AudioProcessorValueTreeState::ParameterLayout BasicMBCompAudioProcessor::createP
     {
         sa.add(String(choice, 1));
     }
-    layout.add(std::make_unique<AudioParameterChoice>("Ratio", "Ratio", sa, 3));
+    layout.add(std::make_unique<AudioParameterChoice>(params.at(Names::Ratio_Low_Band), params.at(Names::Ratio_Low_Band), sa, 3));
 
-    layout.add(std::make_unique<AudioParameterBool>("Bypassed", "Bypassed", false));
+    layout.add(std::make_unique<AudioParameterBool>(params.at(Names::Bypassed_Low_Band), params.at(Names::Bypassed_Low_Band), false));
 
     return layout;
 }
